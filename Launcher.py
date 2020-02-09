9cc86634362c060ec8be02fc846ecd34cac12a30
Pedro Barrera <pedroenri203@gmail.com>
2020-02-09 12:24:51 -0500

    JM100

--- Launcher.py
+++ Launcher.py
@@ -13,6 +13,8 @@ import Game
 
 def main():
     '''aqui iria el codigo de mancera, si tuviera uno!!! D:<'''
+    print("Digite uno de las siguientes clases:")
+    print(" Mago \n Caballero \n Orco \n Aldeano \n Troll")
     Game.main(SELECT_PLAYER = input())
     pass
 
