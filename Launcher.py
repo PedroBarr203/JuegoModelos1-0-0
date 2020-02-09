006c2c6c2f8a5b164abb5e73a145074214808667
Pedro Barrera <pedroenri203@gmail.com>
2020-02-09 11:57:42 -0500

    JM100

---
+++ Launcher.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        módulo1
+# Purpose:
+#
+# Author:      lenovo
+#
+# Created:     30/10/2019
+# Copyright:   (c) lenovo 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import Game
+
+def main():
+    '''aqui iria el codigo de mancera, si tuviera uno!!! D:<'''
+    Game.main(SELECT_PLAYER = input())
+    pass
+
+if __name__ == '__main__':
+    main()
